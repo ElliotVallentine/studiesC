d713783b287d78cd223b7ef3bbece9d20f654a29
ElliotVallentine <marianoferreira.fbs@gmail.com>
2022-12-26 15:31:48 -0300

    Primitive types and data output

---
+++ day-2/ex001/dataOutput.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("valor inteiro: %d.\n",10);
+    printf("valor flutuante %f.\n",10.923283 );
+    printf("valor flutuante com apenas três casas %.3f\n", 10.9018382751678902);
+    printf("dado de texto: %c, %c, %c\n",'a', 'b', 'c');
+    printf("dado de texto: %s\n", "teste");
+}
